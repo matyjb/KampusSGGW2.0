eea635b5d0c4e3c6180251d3f648779d012ba312
Katarzyna Podkowa <kasia-podkowa@wp.pl>
2019-12-07 17:59:38 +0100

    Floating action button

--- kampus_sggw/lib/pages/home_page.dart
+++ kampus_sggw/lib/pages/home_page.dart
@@ -13,6 +13,37 @@ class HomePage extends StatelessWidget {
         onPressed: () => Navigator.pushNamed(context, '/about'),
         child: Text("about"),
       ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FloatingActionButton(
+              onPressed: () {},
+              foregroundColor: Colors.green[500],
+              backgroundColor: Colors.white,
+              child: Icon(
+                Icons.my_location,
+                size: 30,
+              ),
+            ),
+          ),
+          Transform.scale(
+            scale: 1.3,
+            child: Padding(
+              padding: const EdgeInsets.all(13.0),
+              child: FloatingActionButton(
+                backgroundColor: Colors.green[500],
+                child: Icon(
+                  Icons.search,
+                  size: 35,
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
