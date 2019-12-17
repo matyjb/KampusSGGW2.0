363e0544de28a33f888a386d7c15b5a78941d7f7
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2019-12-17 18:17:41 +0100

    todo

--- kampus_sggw/assets/JsonScript.js
+++ kampus_sggw/assets/JsonScript.js
@@ -1,26 +1,14 @@
 const fs = require('fs');
+var oldjson = require("./budynki.json");
 
-let json = fs.readFileSync('NewOne.json');
-let data = JSON.parse(json);
+var newjson = oldjson.map(e => {
+  return {
+    // tu musisz dać tą reprezentacje jednego budynku co dałem na githubie
+    // w starym jsonie tam gdzie jest descriptions.text to ten text nie jest zawsze tablicą więc użyj Array.isArray(object) do sprawdzenia
+    // i jak nie jest to zrob z tego tablice z tym jednym "miejscem",
+    // jak jest to uzyj .map do przemapowania "text" na tablice z obiektami takimi jak na githubie dałem
+  }
+});
 
-class Object
-{
-    constructor(id,name,places,lat,lon)
-    {
-        this.id=id;
-        this.name=name;
-        this.places=places;
-        this.lat=Number(lat);
-        this.lon=Number(lon);
-        for (let i = 0; i < places.length; i++) {
-            this.places[i].lat=Number(places[i].lat);
-            this.places[i].lon=Number(places[i].lon);
-        }
-    }
-}
-
-function GetObject(Id) {
-    return Obj = new Object(data[Id].id,data[Id].name,data[Id].places,data[Id].lat,data[Id].lon);
-}
-
-console.log(GetObject(0));
+var jsonContent = JSON.stringify(newjson);
+fs.writeFile("budynki2.json", jsonContent, ()=>{});
