f81217cdde4efdf6fa980f01a25ede0ea9a59fd6
Jozef <joz.stocki@wp.pl>
2019-12-19 00:48:40 +0100

    I think it's finally done

--- kampus_sggw/assets/JsonScript.js
+++ kampus_sggw/assets/JsonScript.js
@@ -9,71 +9,22 @@ var newjson = oldjson.map(e => {
     lon: e.position.longitude,
     places: Array.isArray(e.descriptions.text) ? e.descriptions.text.map(
       el => {
-      name = el;
-      let lat = 0;
-      let lon = 0;
-      let floor = "0";
       return {
-        name,
-        lat,
-        lon,
-        floor
+        name: el,
+        lat: null,
+        lon: null,
+        floor: null
       };
       }) :
-      () => {
-          name=e.descriptions.text;
-          let lat=0;
-          let lon=0;
-          let floor="0";
-        return [{
-          name,
-          lat,
-          lon,
-          floor
-        }];
-      }
+      [
+        {
+          name: e.descriptions.text,
+          lat: null,
+          lon: null,
+          floor: null
+        }
+      ]
   }
 })
-/*
-var newjson = oldjson.map(e => {
-  if (Array.isArray(e.descriptions.text)) {
-    var places = e.descriptions.text.map(el => {
-      name = el;
-      let lat = 0;
-      let lon = 0;
-      let floor = "0";
-      return {
-        name,
-        lat,
-        lon,
-        floor
-      };
-    });
-  }
-  else {
-    name = e.descriptions.text;
-    let lat = 0;
-    let lon = 0;
-    let floor = "0";
-    places = {
-      name,
-      lat,
-      lon,
-      floor
-    };
-  }
-  id = e.id;
-  name = e.name;
-  lat = Number(e.position.latitude);
-  lon = Number(e.position.longitude);
-  return {
-    id,
-    name,
-    places,
-    lat,
-    lon
-  }
-});
-*/
 var jsonContent = JSON.stringify(newjson);
 fs.writeFile("budynki2.json", jsonContent, () => { });
