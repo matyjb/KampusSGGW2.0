02c40a31b13e7d4b27191dc0496af8828e5e6f57
WiktorElAttar <wiktor.elattar@gmail.com>
2019-12-20 16:11:10 +0100

    created buildings_list_page

--- kampus_sggw/lib/main.dart
+++ kampus_sggw/lib/main.dart
@@ -1,10 +1,9 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kampus_sggw/bloc/buildings_bloc.dart';
 import 'package:kampus_sggw/pages/about_page.dart';
 import 'package:kampus_sggw/pages/buildings_list_page.dart';
 import 'package:kampus_sggw/pages/home_page.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,13 +11,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomePage(),
-        '/about': (context) => AboutPage(),
-        '/buildingsList': (context) => BuildingsListPage()
-      },
+    return BlocProvider<BuildingsBloc>(
+      create: (ctx) => BuildingsBloc(),
+      child: MaterialApp(
+        initialRoute: '/',
+        routes: {
+          '/': (context) => HomePage(),
+          '/about': (context) => AboutPage(),
+          '/buildingsList': (context) => BuildingsListPage()
+        },
+      ),
     );
   }
 }
